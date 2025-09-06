f3b9dbb00de4fc77883031ad999c7c6c0ebae770
Stephen C. Cantoria <stephen.cantoria@thisbyte.com>
2025-09-05 22:16:16 -0400

    angular-json cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
