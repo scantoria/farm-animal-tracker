00ebd6849b428bf7e3ce99caf36a638ff19c309c
Stephen C. Cantoria <stephen.cantoria@thisbyte.com>
2025-09-08 15:22:08 -0400

    ui-ux-validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrls: ['./app.scss'],
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'farm-animal-tracker';
+export class AppComponent implements OnInit {
+  isAuthenticated$: Observable<any>;
+  title = 'FarmAnimalTracker';
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.isAuthenticated$ = this.authService.currentUser$;
+  }
+
+  ngOnInit(): void {
+    this.isAuthenticated$ = this.authService.currentUser$;
+  }
+
+  onLogout(): void {
+    this.authService.signOut()
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/login']);
+        }
+      });
+  }
 }
